465738cb065253cc7e12071a8a2eaf1ce7e8fb49
oxcode <mrexcelsam1@live.com>
2025-09-12 01:10:19 +0100

    setup

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,5 +1,6 @@
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
+import { ProductCad } from '@/components/ProductCad'
 import { BenefitSection } from '@/sections/BenefitSection'
 import { HeartIcon, ShoppingCartIcon } from '@heroicons/react/20/solid'
 import { Quicksand } from 'next/font/google'
@@ -58,36 +59,16 @@ export const page = () => {
       <div className="container w-full mx-auto py-8">
         <div className="flex w-full flex-wrap md:flex-nowrap">
           <div className="w-full md:w-4/5 md:pr-6">
+            <div className="space-y-2 pb-2">
+              <h3 className="font-semibold">
+                Popular Products
+              </h3>
+            </div>
+
             <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 md:gap-6 sm:gap-4">
             { Array.from({ length: 20 }).map((item, key) => (
                 <div key={key}>
-                  <div className="product-card flex flex-col justify-center rounded-md border border-gray-300">
-                    <div className="flex flex-col justify-center items-center">
-                      <img 
-                        src="https://preview.colorlib.com/theme/vegefoods/images/product-2.jpg"
-                        width={160}
-                        alt='Product Image'
-                        height={160}
-                      />
-                    </div>
-                    <div className="flex flex-col justify-center items-center p-2 py-6">
-                      <p className="text-gray-800 font-semibold text-lg">
-                        Bell Pepper
-                      </p>
-                      <p className="cart-text text-gray-400 text-md font-medium">
-                        $100.00
-                      </p>
-
-                      <div className="inline-flex space-x-3 py-1 cart-btn">
-                        <a href="#" className="inline-flex justify-center items-center bg-green-600 text-white size-6.5 p-1 rounded-full">
-                          <ShoppingCartIcon className="size-3.5" />
-                        </a>
-                        <a href="#" className="inline-flex justify-center items-center bg-green-600 text-white size-6.5 p-1 rounded-full">
-                          <HeartIcon className="size-3.5" />
-                        </a>
-                      </div>
-                    </div>
-                  </div>
+                  <ProductCad />
                 </div>
             ))}
             </div>
