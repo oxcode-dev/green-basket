ade328af715d969612b02cf68b183c3a14d21bb5
oxcode <mrexcelsam1@live.com>
2025-09-13 18:20:55 +0100

    setup

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <div className="w-full container mx-auto md:py-16">
+            <div>page</div>
+        </div>
+    )
+}
+
+
+export default page;
