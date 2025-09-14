6b34dd3638ed67cb0e772b678cdb789424996284
oxcode <mrexcelsam1@live.com>
2025-09-14 12:13:03 +0100

    setup

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -6,8 +6,15 @@ const page = () => {
     return (
         <div>
             <div className="container w-full mx-auto py-8 px-4 md:px-0">
+                <div className="w-full min-h-28 rounded-xl bg-center flex items-center" style={{ backgroundImage: "url(/shop-hero.svg)"}}>
+                    <div className="p-4 sm:px-8 md:px-12 py-4">
+                        <p className="font-semibold text-xl md:text-3xl">
+                            All Products
+                        </p>
+                    </div>
+                </div>
 
-                <img src='/shop-hero.svg' />
+                {/* <img src='/shop-hero.svg' /> */}
 
             </div>
 
