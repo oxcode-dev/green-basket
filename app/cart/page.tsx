0f19dcdf0ff37478f389aa7b981bd9adbd39a8f5
oxcode <mrexcelsam1@live.com>
2025-09-15 21:50:04 +0100

    setup

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -13,9 +13,54 @@ const page = () => {
                 </div>
 
                 <div className="w-full py-4 md:py-8">
-                    <div className="border border-gray-300 rounded-lg p-4 md:p-8 w-full">
+                    <div className="flex flex-wrap md:flex-nowrap">
+                        <div className="w-full md:w-2/3 md:pr-6">
+                            <div className="border border-gray-300 rounded-xl p-2 w-full">
+                                <div className="overflow-x-auto">
+                                    <table className="table">
+                                        {/* head */}
+                                        <thead className="text-gray-500">
+                                            <tr>
+                                                <th>Product</th>
+                                                <th>Quantity</th>
+                                                <th>Total</th>
+                                                <th>Action</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <th>
+                                                    <div>
+                                                        <div>
+                                                            <img 
+                                                                src="https://preview.colorlib.com/theme/vegefoods/images/product-2.jpg"
+                                                                width={160}
+                                                                alt='Product Image'
+                                                                height={160}
+                                                            />
+                                                        </div>
+                                                    </div>
+                                                    <div>
 
+                                                    </div>
+                                                </th>
+                                                <td>Cy Ganderton</td>
+                                                <td>$102</td>
+                                                <td>Blue</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="w-full md:w-1/3 md:pl-2">
+                            <div className="border border-gray-300 rounded-lg p-4 md:p-8 w-full">
+
+                            </div>
+                        </div>
                     </div>
+                    
                 </div>
             </div>
         </div>
