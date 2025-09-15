6b0d2a28298a39796be2cf063ccc90e23f3ae66a
oxcode <mrexcelsam1@live.com>
2025-09-16 00:29:22 +0100

    setup

--- app/dashboard/checkout/page.tsx
+++ app/dashboard/checkout/page.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const page = () => {
     return (
-        <div>page</div>
+        <div>
+            <div className="container w-full mx-auto py-8 px-4 md:px-0">
+                Checkout 
+            </div>
+        </div>
     )
 }
 
