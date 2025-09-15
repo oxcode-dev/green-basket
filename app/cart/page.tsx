9c7db0bb12302fd1ed063c342b4e957fe8ce6862
oxcode <mrexcelsam1@live.com>
2025-09-15 22:57:26 +0100

    setup

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -17,7 +17,7 @@ const page = () => {
                     <div className="flex flex-wrap md:flex-nowrap">
                         <div className="w-full md:w-2/3 md:pr-6">
                             <div className="border border-gray-300 rounded-xl p-2 w-full">
-                                <div className="overflow-x-auto">
+                                <div className="overflow-x-auto hidden md:flex">
                                     <table className="table">
                                         {/* head */}
                                         <thead className="text-gray-500">
@@ -38,21 +38,18 @@ const page = () => {
                                                                 width={80}
                                                                 alt='Product Image'
                                                                 height={80}
-                                                                className="size-12 object-cover"
+                                                                className="size-12 object-cover rounded-md"
                                                             />
                                                             <div>
-                                                                <p className="font-semibold text-lg">
+                                                                <p className="font-semibold text-gray-800">
                                                                     Product Title Name
                                                                     Product
                                                                 </p>
-                                                                <p>
+                                                                <p className="font-semibold text-gray-500">
                                                                     $30
                                                                 </p>
                                                             </div>
                                                         </div>
-                                                    </div>
-                                                    <div>
-
                                                     </div>
                                                 </td>
                                                 <td className="px-1 py-2 border-t border-gray-200 bg-white text-md text-center">
@@ -75,6 +72,44 @@ const page = () => {
                                             </tr>
                                         </tbody>
                                     </table>
+                                </div>
+
+                                <div className="md:hidden">
+                                    <p className="font-semibold text-gray-800 text-2xl py-2">Your Cart</p>
+                                    <div>
+                                        <div>
+                                            <div className="inline-flex space-x-3 items-center">
+                                                <img 
+                                                    src="https://preview.colorlib.com/theme/vegefoods/images/product-2.jpg"
+                                                    width={80}
+                                                    alt='Product Image'
+                                                    height={80}
+                                                    className="size-12 object-cover rounded-md"
+                                                />
+                                                <div>
+                                                    <p className="font-semibold text-gray-800">
+                                                        Product Title Name
+                                                        Product
+                                                    </p>
+                                                    <p className="font-semibold text-gray-500">
+                                                        $30
+                                                    </p>
+                                                    <div className="flex justify-between items-center">
+                                                        <div className="inline-flex rounded-full p-0.5 border border-gray-400 text-gray-600">
+                                                            <button type="button" className="text-gray-800 font-semibold p-0.5 px-1.5 cursor-pointer">
+                                                                <MinusIcon className="size-4" />
+                                                            </button>
+                                                            <p className="text-md font-semibold w-4">3</p>
+                                                            <button type="button" className="text-gray-800 font-semibold p-0.5 px-1.5 cursor-pointer">
+                                                                <PlusIcon className="size-4" />
+                                                            </button>
+                                                        </div>
+                                                        <p className="px-1 py-2 font-bold text-md text-center">$102</p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
