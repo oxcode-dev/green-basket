fddae046477eb5c7302c54e4aa42a104305b76de
oxcode <mrexcelsam1@live.com>
2025-09-15 01:40:28 +0100

    setup

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -3,7 +3,15 @@ import React from 'react'
 const page = () => {
     return (
         <div>
-            <cart
+            <div className="container w-full mx-auto py-8 px-4 md:px-0">
+                <div className="w-full min-h-28 rounded-xl bg-center flex items-center" style={{ backgroundImage: "url(/shop-hero.svg)"}}>
+                    <div className="p-4 sm:px-8 md:px-12 py-4">
+                        <p className="font-semibold text-xl md:text-3xl">
+                            All Products
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
