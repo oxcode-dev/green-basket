e5b588cb2e0b9deda363fa8acea7a26033d9231c
oxcode <mrexcelsam1@live.com>
2025-09-15 23:31:51 +0100

    setup

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -29,10 +29,11 @@ const page = () => {
                                             </tr>
                                         </thead>
                                         <tbody>
-                                            <tr className="">
+                                        { Array.from({ length: 4 }).map((item, key) => (
+                                            <tr key={key} className="">
                                                 <td className="px-1 py-2 border-t border-gray-200 bg-white">
                                                     <div>
-                                                        <div className="inline-flex space-x-3 items-center">
+                                                        <div className="inline-flex space-x-3 items-start">
                                                             <img 
                                                                 src="https://preview.colorlib.com/theme/vegefoods/images/product-2.jpg"
                                                                 width={80}
@@ -70,6 +71,7 @@ const page = () => {
                                                     </a>
                                                 </td>
                                             </tr>
+                                        ))}
                                         </tbody>
                                     </table>
                                 </div>
@@ -77,17 +79,19 @@ const page = () => {
                                 <div className="md:hidden">
                                     <p className="font-semibold text-gray-800 text-2xl py-2">Your Cart</p>
                                     <div>
-                                        <div>
-                                            <div className="inline-flex space-x-3 items-center">
+                                    { Array.from({ length: 4 }).map((item, key) => (
+                                        <div key={key} className="py-1.5 border-t border-gray-300">
+                                            <div className="inline-flex space-x-3 items-start">
                                                 <img 
                                                     src="https://preview.colorlib.com/theme/vegefoods/images/product-2.jpg"
                                                     width={80}
                                                     alt='Product Image'
                                                     height={80}
-                                                    className="size-12 object-cover rounded-md"
+                                                    className="size-16 object-cover rounded-md"
                                                 />
-                                                <div>
+                                                <div className="w-full">
                                                     <p className="font-semibold text-gray-800">
+                                                        Product Title Name
                                                         Product Title Name
                                                         Product
                                                     </p>
@@ -95,28 +99,64 @@ const page = () => {
                                                         $30
                                                     </p>
                                                     <div className="flex justify-between items-center">
-                                                        <div className="inline-flex rounded-full p-0.5 border border-gray-400 text-gray-600">
-                                                            <button type="button" className="text-gray-800 font-semibold p-0.5 px-1.5 cursor-pointer">
-                                                                <MinusIcon className="size-4" />
-                                                            </button>
-                                                            <p className="text-md font-semibold w-4">3</p>
-                                                            <button type="button" className="text-gray-800 font-semibold p-0.5 px-1.5 cursor-pointer">
-                                                                <PlusIcon className="size-4" />
-                                                            </button>
+                                                        <div className="flex items-center">
+                                                            <div className="inline-flex rounded-full p-0.5 border border-gray-400 text-gray-600">
+                                                                <button type="button" className="text-gray-800 font-semibold p-0.5 px-1.5 cursor-pointer">
+                                                                    <MinusIcon className="size-4" />
+                                                                </button>
+                                                                <p className="text-md font-semibold w-4">3</p>
+                                                                <button type="button" className="text-gray-800 font-semibold p-0.5 px-1.5 cursor-pointer">
+                                                                    <PlusIcon className="size-4" />
+                                                                </button>
+                                                            </div>
+                                                            <a className="text-gray-600 p-1 inline-flex justify-center" href="#">
+                                                                <TrashIcon className="size-5" />
+                                                            </a>
                                                         </div>
-                                                        <p className="px-1 py-2 font-bold text-md text-center">$102</p>
+                                                        <p className="px-1 py-2 font-bold text-lg text-center">$102</p>
                                                     </div>
                                                 </div>
                                             </div>
                                         </div>
+                                    ))}
                                     </div>
                                 </div>
                             </div>
                         </div>
 
                         <div className="w-full md:w-1/3 md:pl-2">
-                            <div className="border border-gray-300 rounded-lg p-4 md:p-8 w-full">
+                            <div className="border border-gray-300 rounded-lg p-4 w-full">
+                                <div className="w-full">
+                                    <p>
+                                        Order Summary
+                                    </p>
 
+                                    <dl>
+                                        <div className="w-full inline-flex justify-between items-center">
+                                            <dt className="text-sm text-gray-500 font-normal">Sub Total</dt>
+                                            <dd className="text-sm text-gray-800 font-medium">200NGN</dd>
+                                        </div>
+                                        <div className="w-full inline-flex justify-between items-center">
+                                            <dt className="text-sm text-gray-500 font-normal">Tax</dt>
+                                            <dd className="text-sm text-gray-800 font-medium">200NGN</dd>
+                                        </div>
+                                        {/* <div className="w-full inline-flex justify-between items-center">
+                                            <dt className="text-sm text-gray-500 font-normal">Delivery Fee</dt>
+                                            <dd className="text-sm text-gray-800 font-medium">200</dd>
+                                        </div> */}
+                                    </dl>
+
+                                    <div className="w-full inline-flex justify-between items-center pt-2 border-t border-gray-300">
+                                        <dt className="text-md text-gray-600 font-normal">Total</dt>
+                                        <dd className="text-xl text-gray-800 font-medium">$200NGN</dd>
+                                    </div>
+
+                                    <div>
+                                        <p className="text-sm text-gray-500 font-normal">
+                                            Shipping cost will be calculate at checkout
+                                        </p>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
