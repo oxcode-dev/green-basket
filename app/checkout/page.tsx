7fc5f762eeaa8ef888c0adb50b1a4975389bcd4f
oxcode <mrexcelsam1@live.com>
2025-09-17 00:55:20 +0100

    setup

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -10,10 +10,12 @@ const page = () => {
                     </div>
 
                     <div className="w-full md:w-1/3 md:pl-2">
-                        <div className="">
+                        <div className="space-y-4">
                             <div className="border border-gray-300 rounded-xl p-2 w-full">
-                                <p className="font-semibold text-gray-800 text-2xl py-2">Your Cart</p>
-                                { Array.from({ length: 4 }).map((item, key) => (
+                                <p className="text-md text-gray-600 font-semibold py-2">
+                                    Order Summary
+                                </p>
+                                { Array.from({ length: 3 }).map((item, key) => (
                                     <div key={key} className="py-1.5 border-t border-gray-300">
                                         <div className="inline-flex space-x-3 items-start">
                                             <img 
@@ -30,7 +32,7 @@ const page = () => {
                                                 </p>
                                                 
                                                 <div className="flex justify-between items-center">
-                                                    <p>
+                                                    <p className="font-medium text-xs text-gray-500">
                                                         <span>Qty:</span>
                                                         <span>2</span>
                                                     </p>
@@ -43,6 +45,36 @@ const page = () => {
                                         </div>
                                     </div>
                                 ))}
+                            </div>
+
+                            <div className="border border-gray-300 rounded-lg p-4 w-full">
+                                <div className="w-full">
+                                    <dl className="space-y-1">
+                                        <div className="w-full inline-flex justify-between items-center">
+                                            <dt className="text-sm text-gray-500 font-normal">Sub Total</dt>
+                                            <dd className="text-sm text-gray-800 font-medium">200NGN</dd>
+                                        </div>
+                                        <div className="w-full inline-flex justify-between items-center">
+                                            <dt className="text-sm text-gray-500 font-normal">Tax</dt>
+                                            <dd className="text-sm text-gray-800 font-medium">200NGN</dd>
+                                        </div>
+                                        <div className="w-full inline-flex justify-between items-center">
+                                            <dt className="text-sm text-gray-500 font-normal">Delivery Fee</dt>
+                                            <dd className="text-sm text-gray-800 font-medium">200</dd>
+                                        </div>
+                                    </dl>
+
+                                    <div className="w-full inline-flex justify-between items-center pt-2 border-t border-gray-300">
+                                        <dt className="text-md text-gray-600 font-normal">Total</dt>
+                                        <dd className="text-xl text-gray-800 font-medium">$200NGN</dd>
+                                    </div>
+
+                                    <div className="pt-6">
+                                        <button className="btn btn-md bg-black text-white border-black w-full rounded-full">
+                                            Place Order
+                                        </button>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
