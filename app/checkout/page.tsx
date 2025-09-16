391a45ea323e7fc6505fe6ef3f13402d2a57e09e
oxcode <mrexcelsam1@live.com>
2025-09-17 00:38:17 +0100

    setup

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -4,7 +4,15 @@ const page = () => {
     return (
         <div>
             <div className="container w-full mx-auto py-8 px-4 md:px-0">
-                Checkout 
+                <div className="w-full flex flex-wrap md:flex-nowrap items-start space-y-6 md:space-y-0">
+                    <div className="w-full md:w-2/3 md:pr-6">
+
+                    </div>
+
+                    <div className="w-full md:w-2/3 md:pl-2">
+
+                    </div>
+                </div>
             </div>
         </div>
     )
