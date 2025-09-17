a0b37027e01177e4e1b66b2766d2c417db370b94
oxcode <mrexcelsam1@live.com>
2025-09-17 23:31:45 +0100

    setup

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import { Logo } from "@/components/Logo";
+import Link from "next/link";
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="">
+      <div className="w-full border-b border-gray-200 shadow">
+        <div className="w-full container mx-auto flex justify-between items-center">
+          <div className="py-3">
+            <Link href="/">
+              <Logo />
+            </Link>
+          </div>
+
+          <div>
+            <Link href="/cart" className="uppercase font-medium text-sm">
+              Back to Shopping Cart
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      {children}
+    </div>
+  );
+}
