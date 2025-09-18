3e2657d0852db9433acab46ed5b5fe68f3dd6d95
oxcode <mrexcelsam1@live.com>
2025-09-19 00:19:22 +0100

    setup

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,7 +6,7 @@ const page = () => {
 
             <div className="p-3 md:p-4 border-gray-300 border-b">
                 <div>
-                    <h2>Account Management</h2>
+                    <h2>Account Information</h2>
                 </div>
             </div>
             <div className="py-4 p-6">
