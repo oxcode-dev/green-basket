9ed355b671223b83e2500a64e8d748ffbe691414
oxcode <mrexcelsam1@live.com>
2025-09-18 23:56:34 +0100

    setup

--- app/dashboard/addresses/page.tsx
+++ app/dashboard/addresses/page.tsx
@@ -1,4 +1,4 @@
-import { DevicePhoneMobileIcon, MapPinIcon, UserCircleIcon } from '@heroicons/react/24/outline'
+import { DevicePhoneMobileIcon, MapPinIcon, PencilIcon, TrashIcon, UserCircleIcon } from '@heroicons/react/24/outline'
 import React from 'react'
 
 const page = () => {
@@ -10,32 +10,42 @@ const page = () => {
                     <h2>Delivery Addresses</h2>
                 </div>
             </div>
-            <div className="py-4 p-6">
-                <div className="w-full grid grid-cols-1 md:grid-cols-2 md:gap-x-4">
-                    <div className="border border-green-200 rounded-md">
-                        <div className="flex flex-col space-y-2.5 p-2">
-                            <p className="inline-flex items-center space-x-2 text-xs font-medium">
-                                <UserCircleIcon className="size-4" />
-                                <span>Oxcode</span>
-                            </p>
+            <div className="p-3 md:p-4">
+                <div className="w-full grid grid-cols-1 md:grid-cols-2 md:gap-4">
+                    { Array.from({ length: 3 }).map((item, key) => (
+                        <div key={key} className="border border-gray-200 shadow-sm rounded-md">
+                            <div className="flex flex-col space-y-2.5 p-2 text-gray-500">
+                                <p className="inline-flex items-center space-x-2 text-xs font-medium">
+                                    <UserCircleIcon className="size-4" />
+                                    <span>Oxcode</span>
+                                </p>
 
-                            <p className="inline-flex items-center space-x-2 text-xs font-medium">
-                                <MapPinIcon className="size-4" />
-                                <span>
-                                    <span>Street</span>
-                                    <span>city</span>
-                                    <span>state</span>
-                                    <span>postal_code</span>
-                                </span>
-                            </p>
+                                <p className="inline-flex items-center space-x-2 text-xs font-medium">
+                                    <MapPinIcon className="size-4" />
+                                    <span className="space-x-1.5">
+                                        <span>Street</span>
+                                        <span>city</span>
+                                        <span>state</span>
+                                        <span>postal_code</span>
+                                    </span>
+                                </p>
 
-                            <p className="inline-flex items-center space-x-2 text-xs font-medium">
-                                <DevicePhoneMobileIcon className="size-4" />
-                                <span>+2348079344556</span>
-                            </p>
+                                <p className="inline-flex items-center space-x-2 text-xs font-medium">
+                                    <DevicePhoneMobileIcon className="size-4" />
+                                    <span>+2348079344556</span>
+                                </p>
 
+                            </div>
+                            <div className="border-t border-gray-200 p-2 py-1">
+                                <button>
+                                    <TrashIcon className="size-4" />
+                                </button>
+                                <button>
+                                    <PencilIcon className="size-4" />
+                                </button>
+                            </div>
                         </div>
-                    </div>
+                    ))}
                 </div>
             </div>
         </div>
