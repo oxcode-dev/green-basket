f57b953e3bb6982a905d026a93e45f2b932f1fa6
oxcode <mrexcelsam1@live.com>
2025-09-18 22:00:03 +0100

    setup

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,15 +1,12 @@
-import { DashboardSetup } from '@/setups/DashboardSetup'
 import React from 'react'
 
 const page = () => {
     return (
-        <DashboardSetup>
-            <div className="p-3 md:p-4 border-gray-300 border-b">
-                <div>
-                    <h2>Account Management</h2>
-                </div>
+        <div className="p-3 md:p-4 border-gray-300 border-b">
+            <div>
+                <h2>Account Management</h2>
             </div>
-        </DashboardSetup>
+        </div>
     )
 }
 
