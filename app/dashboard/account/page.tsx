a706e1c76e5dca5b65f1667dd782ea9e1beb3fe4
oxcode <mrexcelsam1@live.com>
2025-09-19 23:51:21 +0100

    setup

--- app/dashboard/account/page.tsx
+++ app/dashboard/account/page.tsx
@@ -1,6 +1,10 @@
-import React from 'react'
+'use client';
+
+import ChangePasswordForm from '@/forms/dashboard/ChangePasswordForm'
+import React, { useState } from 'react'
 
 const page = () => {
+    const [selectedTab, setSelectedTab] = useState<Boolean>(true)
     return (
         <div>
 
@@ -13,33 +17,16 @@ const page = () => {
             <div className="py-4 p-6">
                 <div className="">
                     <div role="tablist" className="tabs tabs-border space-x-4">
-                        <a role="tab" className="text-gray-500 tab">Change Password</a>
-                        <a role="tab" className="tab tab-active text-gray-500">Delete Account</a>
+                        <a onClick={() => setSelectedTab(true)} role="tab" className={`tab ${selectedTab ? 'tab-active' : '!text-gray-500'} hover:!text-gray-800`}>
+                            Change Password
+                        </a>
+                        <a onClick={() => setSelectedTab(false)} role="tab" className={`tab ${!selectedTab ? 'tab-active' : '!text-gray-500'} hover:!text-gray-800`}>
+                            Delete Account
+                        </a>
                     </div>
-                    {/* Change Password */}
-                    <div className="">
-                        <form className="space-y-4 pt-6">
-                            <div className="space-y-1.5 flex flex-col">
-                                <label className="text-sm font-medium text-gray-800" htmlFor="last_name">Current Password</label>
-                                <input type="text" name="last_name" placeholder="Last Name" className="input w-full bg-white border border-gray-300" />
-                            </div>
 
-                            <div className="space-y-1.5 flex flex-col">
-                                <label className="text-sm font-medium text-gray-800" htmlFor="email">New Password</label>
-                                <input type="email" name="email" placeholder="Your Email" className="input w-full bg-white border border-gray-300" />
-                            </div>
-
-                            <div className="space-y-1.5 flex flex-col">
-                                <label className="text-sm font-medium text-gray-800" htmlFor="phone">Confirm Password</label>
-                                <input type="text" name="phone" placeholder="+234 8123 456 7890" className="input w-full bg-white border border-gray-300" />
-                            </div>
-                    
-                            <div className="py-2 pt-6">
-                                <button type="submit" className="btn bg-slate-800 text-white rounded-lg btn-md">
-                                    Save Changes
-                                </button>
-                            </div>
-                        </form>
+                    <div className="w-full md:max-w-xl">
+                        {selectedTab ? <ChangePasswordForm /> : null}
                     </div>
 
                 </div>
