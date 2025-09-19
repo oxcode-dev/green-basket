954eb6aa3d03abef285140970f6167346c6e49f2
oxcode <mrexcelsam1@live.com>
2025-09-19 18:48:24 +0100

    setup

--- app/dashboard/wishlists/page.tsx
+++ app/dashboard/wishlists/page.tsx
@@ -1,4 +1,5 @@
 import { ProductCard } from '@/components/ProductCard'
+import { TrashIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import React from 'react'
 
 const page = () => {
@@ -11,9 +12,14 @@ const page = () => {
                 </div>
             </div>
             <div className="py-4 p-6">
-                <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 md:gap-4">
+                <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 md:gap-4 gap-y-4">
                     { Array.from({ length: 10 }).map((item, key) => (
-                        <div key={key}>
+                        <div className="group relative" key={key}>
+                            <a href="#" title="Remove from Wishlist" className="absolute -top-2 -right-1 flex md:hidden group-hover:flex">
+                                <span className="bg-red-500 text-white rounded-full p-1">
+                                    <TrashIcon className="size-5" />
+                                </span>
+                            </a>
                             <ProductCard />
                         </div>
                     ))}
