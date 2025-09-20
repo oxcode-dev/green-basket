d49aeb5712af1f1abfce6a0be5296dbb8451f6ee
oxcode <mrexcelsam1@live.com>
2025-09-20 23:49:42 +0100

    setup

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -17,15 +17,32 @@ const page = () => {
                     </p>
                 </div>
 
-                <div className="w-full space-y-2">
+                <div className="w-full space-y-4">
+                    <div className="flex items-center space-x-4">
+                        <div className="space-y-1.5 flex flex-col w-full">
+                            <label className="text-sm font-medium text-gray-600" htmlFor="first_name">First Name</label>
+                            <input type="text" name="first_name" placeholder="First Name" className="input w-full bg-white border border-gray-300" />
+                        </div>
+                        <div className="space-y-1.5 flex flex-col w-full pb-1">
+                            <label className="text-sm font-medium text-gray-600" htmlFor="last_name">Last Name</label>
+                            <input type="text" name="last_name" placeholder="Last Name" className="input w-full bg-white border border-gray-300" />
+                        </div>
+                    </div>
                     <div className="space-y-1.5 flex flex-col">
                         <label className="text-sm font-medium text-gray-600" htmlFor="email">Email</label>
                         <input type="email" name="email" placeholder="Your Email" className="input w-full bg-white border border-gray-300" />
                     </div>
 
-                    <div className="space-y-1.5 flex flex-col">
-                        <label className="text-sm font-medium text-gray-600" htmlFor="password">Password</label>
-                        <input type="password" name="password" placeholder="*********" className="input w-full bg-white border border-gray-300" />
+                    <div className="flex items-center space-x-4">
+                        <div className="space-y-1.5 flex flex-col w-full">
+                            <label className="text-sm font-medium text-gray-600" htmlFor="password">Password</label>
+                            <input type="password" name="password" placeholder="*********" className="input w-full bg-white border border-gray-300" />
+                        </div>
+
+                        <div className="space-y-1.5 flex flex-col w-full">
+                            <label className="text-sm font-medium text-gray-600" htmlFor="confirm_password">Confirm Password</label>
+                            <input type="password" name="confirm_password" placeholder="*********" className="input w-full bg-white border border-gray-300" />
+                        </div>
                     </div>
 
                     <div className="flex justify-between items-center">
