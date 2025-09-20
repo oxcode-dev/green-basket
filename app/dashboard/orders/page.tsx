0299b5a8e5f34efe0455ab920392a13c894ee971
oxcode <mrexcelsam1@live.com>
2025-09-20 07:58:55 +0100

    setup

--- app/dashboard/orders/page.tsx
+++ app/dashboard/orders/page.tsx
@@ -10,8 +10,20 @@ const page = () => {
                 </div>
             </div>
             <div className="py-4 p-6">
-                <div className="">
-                    Hello
+                <div className="space-y-4">
+                    { Array.from({ length: 6 }).map((item, key) => (
+
+                        <div className="card w-full bg-white border border-gray-300 card-sm shadow-sm">
+                            <div className="card-body">
+                                <h2 className="card-title">Xsmall Card</h2>
+                                <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                                <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                                <div className="justify-end card-actions">
+                                    <button className="btn btn-primary">Buy Now</button>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
