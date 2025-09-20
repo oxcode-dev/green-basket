d00a305f3b3458d6d42edce21d1a2319e1510caf
oxcode <mrexcelsam1@live.com>
2025-09-20 15:02:54 +0100

    setup

--- app/dashboard/orders/page.tsx
+++ app/dashboard/orders/page.tsx
@@ -1,3 +1,4 @@
+import { DevicePhoneMobileIcon, MapPinIcon, UserCircleIcon } from '@heroicons/react/24/outline'
 import React from 'react'
 
 const page = () => {
@@ -44,7 +45,69 @@ const page = () => {
                 </div>
             </div>
 
-            <div className="py-4 p-6">
+            <div className="py-4 p-6 space-y-4">
+                <div>
+                    <div className="flex justify-between items-center pb-2 border-b border-gray-200">
+                        <p className="text-xl font-semibold">
+                            Order Information
+                        </p>
+                        {/* <p></p> */}
+                    </div>
+
+                    <dl className="space-y-2 divide-y divide-gray-200 text-gray-500 pt-2">
+                        <div className="flex justify-between items-center">
+                            <dt className="font-medium">Order Number</dt>
+                            <dd>HuJ2320/4345</dd>
+                        </div>
+                        <div className="flex justify-between items-center">
+                            <dt className="font-medium">Order Date</dt>
+                            <dd>11 Dec 2024</dd>
+                        </div>
+                        <div className="flex justify-between items-center">
+                            <dt className="font-medium">Delivery Fee</dt>
+                            <dd>#3000</dd>
+                        </div>
+                        <div className="flex justify-between items-center">
+                            <dt className="font-medium">Items Total</dt>
+                            <dd>#20000</dd>
+                        </div>
+                        <div className="flex justify-between items-center text-gray-700">
+                            <dt className="font-semibold">Total Amount</dt>
+                            <dd className="font-semibold">#200000</dd>
+                        </div>
+                    </dl>
+                </div>
+                <div>
+                    <div className="flex justify-between items-center pb-2 border-b border-gray-200">
+                        <p className="text-xl font-semibold">
+                            Delivery Information
+                        </p>
+                        {/* <p></p> */}
+                    </div>
+
+                    <div className="flex flex-col space-y-2.5 p-2 text-gray-500">
+                        <p className="inline-flex items-center space-x-2 text-sm font-medium">
+                            <UserCircleIcon className="size-4" />
+                            <span>Oxcode</span>
+                        </p>
+
+                        <p className="inline-flex items-center space-x-2 text-sm font-medium">
+                            <MapPinIcon className="size-4" />
+                            <span className="space-x-1.5">
+                                <span>Street</span>
+                                <span>city</span>
+                                <span>state</span>
+                                <span>postal_code</span>
+                            </span>
+                        </p>
+
+                        <p className="inline-flex items-center space-x-2 text-sm font-medium">
+                            <DevicePhoneMobileIcon className="size-4" />
+                            <span>+2348079344556</span>
+                        </p>
+
+                    </div>
+                </div>
             </div>
         </div>
 
