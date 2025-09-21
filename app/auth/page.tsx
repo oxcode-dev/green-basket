28e62463faaf43527cbd6e35834ef38af95f4cd2
oxcode <mrexcelsam1@live.com>
2025-09-21 22:29:43 +0100

    setup

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { LoginForm } from '@/forms/auth/LoginForm'
 import Link from 'next/link'
 import React from 'react'
 
@@ -18,37 +19,7 @@ const page = () => {
                 </div>
 
                 <div className="w-full space-y-2">
-                    <div className="space-y-1.5 flex flex-col">
-                        <label className="text-sm font-medium text-gray-600" htmlFor="email">Email</label>
-                        <input type="email" name="email" placeholder="Your Email" className="input w-full bg-white border border-gray-300" />
-                    </div>
-
-                    <div className="space-y-1.5 flex flex-col">
-                        <label className="text-sm font-medium text-gray-600" htmlFor="password">Password</label>
-                        <input type="password" name="password" placeholder="*********" className="input w-full bg-white border border-gray-300" />
-                    </div>
-
-                    <div className="flex justify-between items-center">
-                        <div>
-                            <label className="label text-sm">
-                                <input type="checkbox" defaultChecked className="checkbox checkbox-success checked:text-white" />
-                                Remember me
-                            </label>
-                        </div>
-
-                        <div>
-                            <Link href="#" className="text-sm text-gray-500 font-medium underline">
-                                Forgot Password?
-                            </Link>
-                        </div>
-
-                    </div>
-            
-                    <div className="py-2 pt-6">
-                        <button type="submit" className="btn bg-green-600 border-green-600 text-white btn-md">
-                            Sign In
-                        </button>
-                    </div>
+                    <LoginForm />
 
                     <div className="pt-4 md:pt-6">
                         <p className="text-sm text-gray-500 font-medium">
