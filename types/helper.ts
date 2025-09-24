f202d712407e74e584ba594a2823b5a01dd7d54d
oxcode <mrexcelsam1@live.com>
2025-09-24 23:11:25 +0100

    setup

--- types/helper.ts
+++ types/helper.ts
@@ -9,3 +9,16 @@ export const useInitials = (fullName: string) :string => {
 
     return `${firstInitial}${lastInitial}`.toUpperCase();
 }
+
+export function formatDate(value: string|null) {
+    if(!value) return '';
+    return new Intl.DateTimeFormat('default', { dateStyle: 'long' }).format(
+        new Date(value)
+    )
+}
+
+export function numberFormat(number: number|null) {
+    if(!number) return '';
+
+    return number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,");
+}
