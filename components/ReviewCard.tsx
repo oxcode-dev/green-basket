374ddc6e05bff36cdacbd7fcf4091ca9ad4572c5
oxcode <mrexcelsam1@live.com>
2025-10-01 18:37:31 +0100

    setup

--- components/ReviewCard.tsx
+++ components/ReviewCard.tsx
@@ -8,12 +8,8 @@ export const ReviewCard = () => {
                 <div>
                     <div className="rating">
                         { Array.from({ length: 5 }).map((item, key) => (
-                            <div key={key} className="mask mask-star" aria-label={`${item} star`} aria-current={item === 3}></div>
+                            <div key={key} className="mask mask-star bg-warning" aria-label={`${key} star`} aria-current={key === 4}></div>
                         ))}
-                        {/* <div className="mask mask-star" aria-label="2 star"></div>
-                        <div className="mask mask-star" aria-label="3 star"></div>
-                        <div className="mask mask-star" aria-label="4 star" aria-current="true"></div>
-                        <div className="mask mask-star" aria-label="5 star"></div> */}
                     </div>
                 </div>
                 <p className="py-2 text-gray-600">
